a10945291bc0287ab994c0151e145e190f197373
Mohamad Adib Tawil <mat.07.mat.70.mat@gmail.com>
2025-09-22 16:23:38 +0300

    add appbar for book details

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:code_books/home/domain/entities/book_entity.dart';
 import 'package:go_router/go_router.dart';
 
@@ -24,33 +25,19 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kBookDetailsView,
-        // builder: (context, state) => const BoookDetailsView(),
-        builder: (context, state) {
+        pageBuilder: (context, state) {
           final book = state.extra as BookEntity;
-          return BoookDetailsView(book: book);
+          return CupertinoPage(
+            key: state.pageKey,
+            child: BoookDetailsView(book: book),
+          );
         },
       ),
       GoRoute(
         path: kSearchView,
         pageBuilder: (context, state) {
-          return CustomTransitionPage(
-            key: state.pageKey,
-            child: const SearchView(),
-            transitionDuration: const Duration(milliseconds: 280),
-            reverseTransitionDuration: const Duration(milliseconds: 260),
-            transitionsBuilder: (context, animation, secondaryAnimation, child) {
-              final offsetTween = Tween<Offset>(begin: const Offset(0.1, 0), end: Offset.zero)
-                  .chain(CurveTween(curve: Curves.easeOutCubic));
-              final fadeTween = Tween<double>(begin: 0.0, end: 1.0)
-                  .chain(CurveTween(curve: Curves.easeOut));
-              return FadeTransition(
-                opacity: animation.drive(fadeTween),
-                child: SlideTransition(
-                  position: animation.drive(offsetTween),
-                  child: child,
-                ),
-              );
-            },
+          return const CupertinoPage(
+            child: SearchView(),
           );
         },
       ),
--- lib/home/presentation/views/book_details_view.dart
+++ lib/home/presentation/views/book_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:code_books/home/domain/entities/book_entity.dart';
 import 'package:code_books/home/presentation/views/widgets/book_details_body.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../contants.dart';
 
@@ -12,6 +13,25 @@ class BoookDetailsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kDarkBlackColor,
+      appBar: AppBar(
+        backgroundColor: kDarkBlackColor,
+        elevation: 0,
+        centerTitle: true,
+        leading: IconButton(
+          tooltip: 'Back',
+          icon: const Icon(Icons.arrow_back_ios_new, color: kWhiteColor),
+          onPressed: () => context.pop(),
+        ),
+        title: Text(
+          book.title,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: const TextStyle(
+            color: kPrimaryColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
       body: BookDetailsBody(book: book),
     );
   }
